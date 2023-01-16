ea5598ba6cf8ef87abe2263480a66fef5b18b264
Liran-1 <Liran.Sr@gmail.com>
2023-01-16 15:41:11 +0200

    First Commit.

---
+++ app/src/main/java/com/example/cookbook/auth/UserAuthentication.java
@@ -0,0 +1,83 @@
+package com.example.cookbook.auth;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cookbook.R;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UserAuthentication extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null) {
+            login();
+        } else {
+            String uid = user.getUid();
+            String phone = user.getPhoneNumber();
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            int x = 0;
+        }
+
+//        user.updateProfile(new UserProfileChangeRequest.Builder()
+//                .setDisplayName("Tom Cohen")
+//                .build());
+
+//        String uid = user.getUid();
+//        String phone = user.getPhoneNumber();
+//        String name = user.getDisplayName();
+//        String email = user.getEmail();
+//        int x= 0;
+    }
+
+    // See: https://developer.android.com/training/basics/intents/result
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    onSignInResult(result);
+                }
+            }
+    );
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+    }
+
+
+    private void login() {
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        // Create and launch sign-in intent
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build();
+        signInLauncher.launch(signInIntent);
+    }
+}
